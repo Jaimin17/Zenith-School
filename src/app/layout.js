9d0729a1331c0ecbeed52f8f3f46ca5a24e1335d
Jaimin Bhatt <krishbhatt1217@gmail.com>
2025-09-08 23:28:46 +0530

    updated file structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,15 +4,10 @@ import { CssBaseline, Box } from "@mui/material";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import "./globals.css";
-import "../assets/css/bootstrap.min.css";
-import "../assets/css/all-fontawesome.min.css";
-import "../assets/css/animate.min.css";
-import "../assets/css/magnific-popup.min.css";
-import "../assets/css/owl.carousel.min.css";
-import "../assets/css/style.css";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Loader from "@/components/loader/preloader";
 import SearchPopUp from "@/components/search-popup/SearchPopUp";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -24,6 +19,13 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="" />
         <meta name="keywords" content="" />
+
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/css/all-fontawesome.min.css" />
+        <link rel="stylesheet" href="/assets/css/animate.min.css" />
+        <link rel="stylesheet" href="/assets/css/magnific-popup.min.css" />
+        <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="/assets/css/style.css" />
       </head>
       <body>
         <CssBaseline />
@@ -41,6 +43,20 @@ export default function RootLayout({ children }) {
 
         {/* Footer always at bottom */}
         <Footer />
+
+
+        <Script src="/assets/js/jquery-3.7.1.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/modernizr.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/imagesloaded.pkgd.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/jquery.magnific-popup.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/isotope.pkgd.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/jquery.appear.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/jquery.easing.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/owl.carousel.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/counter-up.js" strategy="afterInteractive" />
+        <Script src="/assets/js/wow.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/main.js" strategy="afterInteractive" />
       </body>
     </html>
   );

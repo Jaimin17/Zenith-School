941688c99ea68cf64252f885889cb82cb67db1aa
Jaimin Bhatt <krishbhatt1217@gmail.com>
2025-09-16 00:00:49 +0530

    error solve

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -3,7 +3,6 @@
 import Banner from "@/components/Banner";
 import ContactUsContainer from "@/components/contactUs/ContactUsContainer";
 import Map from "@/components/Map";
-import { ContactUsForm } from "@/components/ui/contact-us-form";
 import { BANNER_DATA } from "@/lib/data";
 import { Box, Typography, Container } from "@mui/material";
 
